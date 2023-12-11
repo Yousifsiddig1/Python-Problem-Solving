a6e527f8da03275c516d799420e8246785c576b7
Yousif <145899994+Yousifsiddig1@users.noreply.github.com>
2023-12-11 13:42:00 +0200

    Add files via upload

---
+++ Fall 2022 .py
@@ -0,0 +1,14 @@
+def num_List(my_list1):
+    x= int(input("Enter Positive integer or  negative to stop :"))
+    while x >=0:
+        my_list1.append(x)
+        x= int(input("Enter Positive integer or  negative to stop :"))
+    num_list(my_list1)
+def num_list(my_list1):
+    my_list2=[]
+    for  i in my_list1:
+        if i %3==0 or i % 7 ==0:
+            my_list2.append(i)
+    print(my_list2)
+my_list1 =[]
+num_List(my_list1)
