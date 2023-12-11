d1dd54df7996d7e915aac72d07a2017e2b6f5947
Yousif <145899994+Yousifsiddig1@users.noreply.github.com>
2023-12-11 02:18:52 +0200

    spring2022.py

--- spring2022.py
+++ spring2022.py
@@ -1,40 +1,41 @@
 # final spring 22-23
 # part 2 
 # Question 4 
-#def find_quotiant_and_remainder(a,b):
-#    quotient = a // b
-#    remainder = a % b
-#    return  quotient, remainder 
-#a = int(input("Enter a :"))
-#b = int(input("Enter b :"))
-#quotiant , remainder =find_quotiant_and_remainder(a,b)
-#print('Quotiant', quotiant)
-#print('Remainder',remainder)
+def find_quotiant_and_remainder(a,b):
+    quotient = a // b
+    remainder = a % b
+    return  quotient, remainder 
+a = int(input("Enter a :"))
+b = int(input("Enter b :"))
+quotiant , remainder =find_quotiant_and_remainder(a,b)
+print('Quotiant', quotiant)
+print('Remainder',remainder)
 # part 3 
 # Question 1 
-#my_list = [14,12,9,11,19]
-#filename = input("Enter file name :")
-# open the file
-#file = open(filename, 'w') # or with open(filename, 'w') as file: 
-#for i in my_list:
-#    file.write(str(i) + '\n')
-#file.close() # if u used ' with open(filename, 'w') as file:' you dont need to close 
+my_list = [14,12,9,11,19]
+filename = input("Enter file name :")
+  #open the file
+file = open(filename, 'w') # or with open(filename, 'w') as file: 
+for i in my_list:
+    file.write(str(i) + '\n')
+file.close() # if u used ' with open(filename, 'w') as file:' you dont need to close 
 # Question 2 
-#def print_odds(number):
-#    for i in range(number,0,-1):
-#        if i%2 !=0:
-#            print(i,end=' ')
-#def main():
-#    number = int(input("Enter a positive Starting number")) # prompt the user to enter a positive starting nubmer 
-#    while True:
-#        if number <= 0 :
-#            number= int(input("Wrong entry ! Enter a positive starting number "))
-#        else:
-#            print_odds(number)
-#            break
-#main()
-# Question 3
+def print_odds(number):
+    for i in range(number,0,-1):
+        if i%2 !=0:
+            print(i,end=' ')
+def main():
+    number = int(input("Enter a positive Starting number")) # prompt the user to enter a positive starting nubmer 
+    while True:
+        if number <= 0 :
+            number= int(input("Wrong entry ! Enter a positive starting number "))
+        else:
+            print_odds(number)
+            break
+main()
 
+# Question 3
+# Will add soon 
 
 #Question 4
 class student:
